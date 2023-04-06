cf308bc76fd3d8246c2fe11ebc0ae876ec3d8619
FW <843189132@qq.com>
2023-04-06 13:03:19 +0800

    test

--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -1,17 +1,17 @@
 import { createParser } from "eventsource-parser";
-const decoder = new TextDecoder();
-const encoder = new TextEncoder();
+// import { fetch, ProxyAgent } from "undici";
 
-export const get = async (context) => {
-  const params = context.url.searchParams;
-  const text = params.get("input");
+// import HttpsProxyAgent from "https-proxy-agent/dist/agent";
+// import axios from "axios";
+// const httpsAgent = new HttpsProxyAgent({ host: "http://127.0.0.1", port: "7890" })
+// const instance = axios.create(httpsAgent)
+// const decoder = new TextDecoder();
+// const encoder = new TextEncoder();
 
-  // const completion = await openai.createCompletion({
-  //   model: "text-davinci-003",
-  //   prompt: `写出下一句诗。上句：${text}。下句：`,
-  //   temperature: 0.6,
-  //   stream: true,
-  // });
+
+export const get = async () => {
+  // const params = context.url.searchParams;
+  // const text = params.get("input");
 
   const completion = await fetch("https://api.openai.com/v1/completions", {
     headers: {
@@ -20,40 +20,56 @@ export const get = async (context) => {
     },
     method: "POST",
     body: JSON.stringify({
-      model: "text-davinci-003",
-      prompt: `写出下一句诗。上句：${text}。下句：`,
-      temperature: 0.6,
-      stream: true,
+      "model": "text-davinci-003",
+      "prompt": "Say this is not a test",
+      "max_tokens": 10,
+      "temperature": 0
     }),
+    // dispatcher: new ProxyAgent('http://127.0.0.1:7890')
   });
 
-  console.log(completion);
+  // const res = await instance({
+  //   url: "https://api.openai.com/v1/completions",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: `Bearer ${import.meta.env.OPENAI_API_KEY ?? ""}`,
+  //   },
+  //   method: 'POST',
+  //   data: JSON.stringify({
+  //     "model": "text-davinci-003",
+  //     "prompt": "Say this is not a test",
+  //     "max_tokens": 10,
+  //     "temperature": 0
+  //   }),
+  // })
+  // console.log(JSON.stringify(completion));
+  return new Response(JSON.stringify(completion.data))
 
-  const stream = new ReadableStream({
-    async start(controller) {
-      const streamParser = (event) => {
-        if (event.type === "event") {
-          const data = event.data;
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].text;
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-          } catch (e) {
-            console.log(e);
-          }
-        }
-      };
-      const parser = createParser(streamParser);
-      for await (const chunk of completion.body) {
-        parser.feed(decoder.decode(chunk));
-      }
-    },
-  });
+  // const stream = new ReadableStream({
+  //   async start(controller) {
+  //     const streamParser = (event) => {
+  //       if (event.type === "event") {
+  //         const data = event.data;
+  //         if (data === "[DONE]") {
+  //           controller.close();
+  //           return;
+  //         }
+  //         try {
+  //           const json = JSON.parse(data);
+  //           const text = json.choices[0].text;
+  //           const queue = encoder.encode(text);
+  //           controller.enqueue(queue);
+  //         } catch (e) {
+  //           console.log(e);
+  //         }
+  //       }
+  //     };
+  //     const parser = createParser(streamParser);
+  //     for await (const chunk of completion.body) {
+  //       parser.feed(decoder.decode(chunk));
+  //     }
+  //   },
+  // });
 
-  return new Response(stream);
+  // return new Response(stream);
 };
